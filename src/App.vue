366155385fd069637af97849bd07086aac67026e
Joshua L.J. Vachon <joshua.vachon@historiamati.ca>
2021-05-31 11:24:48 -0400

    Menu

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <template>
   <div id="nav">
-    <router-link :to="{name: 'Accueil'}"><span class="material-icons">home</span><span class="titrebtn">Accueil</span></router-link>
-    <router-link :to="{name: 'Sig'}"><span class="material-icons">map</span><span class="titrebtn">SIG</span></router-link>
+    <router-link :to="{name: 'Accueil'}"><img class="icon" src="@/assets/home.svg"/><span class="titrebtn">Accueil</span></router-link>
+    <router-link :to="{name: 'Sig'}"><img class="icon" src="@/assets/globe.svg"/><span class="titrebtn">SIG</span></router-link>
+    <router-link :to="{name: 'Sig'}"><img class="icon" src="@/assets/marker.svg"/><span class="titrebtn">Storymaps 1</span></router-link>
+    <router-link :to="{name: 'Sig'}"><img class="icon" src="@/assets/trajet.svg"/><span class="titrebtn">Storymaps 2</span></router-link>
+    <router-link :to="{name: 'Sig'}"><img class="icon" src="@/assets/reseau.svg"/><span class="titrebtn">Asie</span></router-link>
+    <router-link :to="{name: 'Sig'}"><img class="icon" src="@/assets/livre.svg"/><span class="titrebtn">Bagdadbahn</span></router-link>
+    <router-link :to="{name: 'Sig'}"><img class="icon" src="@/assets/info.svg"/><span class="titrebtn">Information</span></router-link>
   </div>
   <router-view id="wrap"/>
 </template>
